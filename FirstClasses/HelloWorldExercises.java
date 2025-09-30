47358c54ff86a05895f7d342324beedfe24d5024
AndresDev20 <andresmarroquin887@gmail.com>
2025-09-30 18:31:58 -0500

    First Class

--- FirstClasses/HelloWorldExercises.java
+++ FirstClasses/HelloWorldExercises.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class HelloWorldExercises {
 
     public static void main(String[] args) {
         // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+        System.out.println("Yeison Andres Marroquin Bernal");
 
         // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+        System.out.println("Hola\nMundo");
 
         // 3. Añade un comentario sobre lo que hace cada línea del programa.
+        // creo una variable para la edad
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa tu edad: ");
+        int edad = sc.nextInt();
+        //uso una sentencia para determinar si la persona es menor o mayor de edad
+        if (edad >= 18) {
+            System.out.println("Eres Mayor de edad");
+        }else{
+            System.out.println("Eres Menor de edad");
+        }
 
         // 4. Crea un comentario en varias líneas.
+        /* Hello, my name is Andres and I am a programmer,
+        so I study Java on YouTube with MoureDev */
+
 
         // 5. Imprime tu edad, tu color favorito y tu ciudad.
 

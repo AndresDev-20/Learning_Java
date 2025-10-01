e011eba7aeacd16c5359fafa44992c9579386845
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-01 17:52:34 -0500

    DataTypes

---
+++ FirstClasses/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        //Tipos de datos primitivos
+        int myInt = 37;
+        System.out.println(myInt);
+
+        double myDouble = 1.23;
+        System.out.println(myDouble);
+
+
+    }
+}

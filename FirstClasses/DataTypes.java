6dee64211bbe89db49880d02b36e9a02db28a2bf
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-06 15:53:29 -0500

    Trird class

--- FirstClasses/DataTypes.java
+++ FirstClasses/DataTypes.java
@@ -3,11 +3,25 @@ public class DataTypes {
     public static void main(String[] args) {
 
         //Tipos de datos primitivos
+        // para números
         int myInt = 37;
         System.out.println(myInt);
 
         double myDouble = 1.23;
         System.out.println(myDouble);
+        // float, long, byte
+
+        // para caracteres
+        char myChar = 'a';
+
+        // booleanos
+        boolean myBoolean = true;
+
+
+        // Como saber el tipo de dato en compilación para un dato no primitivo
+        String text = "Hi, this is a text for test";
+        System.out.println(text.getClass().getSimpleName());
+
 
 
     }

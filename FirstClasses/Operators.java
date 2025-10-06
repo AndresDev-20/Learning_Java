54bf68d84c26101dd134d01f08d33b2aaee00d78
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-06 17:19:52 -0500

    Update

---
+++ FirstClasses/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+
+    public static void main(String[] args) {
+        //Operadores en Java
+        // Aritméticos
+        var a = 5;
+        var b = 9;
+        System.out.println(a + b); // Suma
+        System.out.println(a - b); // Resta
+        System.out.println(a * b); // Multiplicación
+        System.out.println(a / b); // División
+        System.out.println(a % b); // Modulación
+
+        // Asignación
+
+    }
+}

9f2c26ab774a2e3abd139cea2fa9f32d9197d2f7
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-08 16:40:49 -0500

    methods

--- FirstClasses/Strings.java
+++ FirstClasses/Strings.java
@@ -13,5 +13,15 @@ public class Strings {
         // Hallar la longitud con length()
         String animal = "Gato";
         System.out.println(animal.length());
+
+        // Obtener un carácter con charAt()
+        String person = "Andres.dev";
+        System.out.println(person.charAt(0)); // Asi obtengo el carácter "A".
+        System.out.println(person.charAt(person.length() - 1)); // Para el ultímo puedo usar -1, pero con el método length.
+
+        // Obtener una subcadena usando a substring()
+        System.out.println(person.substring(2)); // con esto le decimos que arranque desde el indice 2 mostrando "dres.dev"
+        System.out.println(person.substring(2, 4)); // y con esto damos un inicio y un fin de lo que queremos extraer del string.
+
     }
 }

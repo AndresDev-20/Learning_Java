eeab8666422b10548f3a88b0d8b5bac2cb75a756
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-10 15:37:50 -0500

    Conditionals

--- FirstClasses/Clase_04/Conditionals.java
+++ FirstClasses/Clase_04/Conditionals.java
@@ -13,5 +13,34 @@ public class Conditionals {
         } else {
             System.out.println("Eres demaciado viejo para este programa");
         }
+
+        //switch
+        var day = 7;
+        switch (day){
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("El dia no existe");
+                break;
+        }
     }
 }

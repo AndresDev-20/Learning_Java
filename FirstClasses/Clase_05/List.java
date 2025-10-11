9f26799fb385573446143822af84d6e837d4d59c
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-11 17:12:21 -0500

    Listas

--- FirstClasses/Clase_05/List.java
+++ FirstClasses/Clase_05/List.java
@@ -1,10 +1,13 @@
 package Clase_05;
 
+import java.util.ArrayList;
+
 public class List {
 
     public static void main(String[] args) {
 
         // Declaración y creación
-
+        ArrayList<String> names = new ArrayList<>();
+        var numbers = new ArrayList<Integer>();
     }
 }

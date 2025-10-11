d2c6d2cd73d84912bdc2c5bb80c77248e767bbfd
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-11 15:49:34 -0500

    Arrays

--- FirstClasses/Clase_05/Arrays.java
+++ FirstClasses/Clase_05/Arrays.java
@@ -4,6 +4,10 @@ public class Arrays {
 
     public static void main(String[] args) {
         //Declaración y creación
+        int[] numbers = new int[4];
+        System.out.println(numbers);
 
+        String[] name = {"Andres", "Yeison", "Marroquin"};
+        System.out.println(name[2]);
     }
 }

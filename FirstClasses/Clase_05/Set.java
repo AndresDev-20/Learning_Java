c43e652acbdadfc5aa92501fa4f31c7ababb1cc5
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-13 12:22:34 -0500

    Sets

--- FirstClasses/Clase_05/Set.java
+++ FirstClasses/Clase_05/Set.java
@@ -9,5 +9,21 @@ public class Set {
         // Declaración y creación
         HashSet<String> fruits = new HashSet<>();
         System.out.println(fruits);
+
+        // Tamaño
+        System.out.println(fruits.size());
+
+        // Añadir elementos
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Strawberry");
+        System.out.println(fruits);
+
+        // Eliminar elementos
+        fruits.remove("Apple");
+        System.out.println(fruits);
+
+        // Buscar elementos
+        System.out.println(fruits.contains(("Banana")));
     }
 }

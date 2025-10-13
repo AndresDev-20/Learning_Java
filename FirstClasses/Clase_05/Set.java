c09ee750def379548ec5eb6c13674e0c8eb4ccea
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-13 17:50:31 -0500

    Sets

--- FirstClasses/Clase_05/Set.java
+++ FirstClasses/Clase_05/Set.java
@@ -1,8 +1,13 @@
 package Clase_05;
 
+import java.util.HashSet;
+
 public class Set {
 
     public static void main(String[] args) {
 
+        // Declaración y creación
+        HashSet<String> fruits = new HashSet<>();
+        System.out.println(fruits);
     }
 }

45b0e50d43a463987c9a552d68b20db10ca3866f
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-15 16:53:29 -0500

    Bucle for

--- FirstClasses/Clase_06/Loops.java
+++ FirstClasses/Clase_06/Loops.java
@@ -4,5 +4,11 @@ public class Loops {
 
     public static void main(String[] args) {
 
+        // Loops - Bucles
+
+        // - for
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hola, Java!");
+        }
     }
 }

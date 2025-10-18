eaaa20e089543acbee1a553c3729eb0e3443ec67
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-18 14:47:30 -0500

    Functions

--- FirstClasses/Clase_07/Functions.java
+++ FirstClasses/Clase_07/Functions.java
@@ -1,4 +1,14 @@
 package Clase_07;
 
 public class Functions {
+
+    public static void main(String[] args) {
+        // Funciones
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hola mundo");
+        }
+
+        // ...
+        System.out.println("Hola Mundo!");
+    }
 }

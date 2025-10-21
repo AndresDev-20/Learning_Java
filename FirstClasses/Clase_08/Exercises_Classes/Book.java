2514b669ca24f48122f11484f2b28fc0c3513f2e
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-21 15:45:51 -0500

    Logic

---
+++ FirstClasses/Clase_08/Exercises_Classes/Book.java
@@ -0,0 +1,14 @@
+package Clase_08.Exercises_Classes;
+
+public class Book {
+
+    // Atributos
+    String title;
+    String autor;
+
+    // Constructor
+    public Book(String title, String autor) {
+        this.title = title;
+        this.autor = autor;
+    }
+}
---
+++ FirstClasses/Clase_08/Exercises_Classes/Dog.java
@@ -0,0 +1,8 @@
+package Clase_08.Exercises_Classes;
+
+public class Dog {
+
+    public void Bark() {
+        System.out.println("Guauuuuu!!!!");
+    }
+}

2b52e057cf4edb1def639c8656ff2dfae99e802a
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-22 10:47:56 -0500

    Encapsulamiento

---
+++ FirstClasses/Clase_08/AccessModifiers.java
@@ -0,0 +1,14 @@
+package Clase_08;
+
+public class AccessModifiers {
+
+    public static void main(String[] args) {
+        // Encapsulamiento
+
+        // - Modificadores de acceso
+        // public
+        // private
+        // protected
+
+    }
+}

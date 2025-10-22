11161d1c88fffc8bf9224a6bb1c0187243bd5256
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-22 12:26:46 -0500

    Logic Java Pooo

---
+++ FirstClasses/Clase_08/Exercises_AccessModifiers/Person.java
@@ -0,0 +1,35 @@
+package Clase_08.Exercises_AccessModifiers;
+
+public class Person {
+
+    // Atributos
+    private String name;
+    private int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Métodos Getter y Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Edad no valida");
+        }
+    }
+}

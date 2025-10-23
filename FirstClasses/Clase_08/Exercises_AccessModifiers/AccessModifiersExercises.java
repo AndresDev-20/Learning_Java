85fa053d8817246b3ffa543e84408174e4502764
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-23 13:33:10 -0500

    Logic

--- FirstClasses/Clase_08/Exercises_AccessModifiers/AccessModifiersExercises.java
+++ FirstClasses/Clase_08/Exercises_AccessModifiers/AccessModifiersExercises.java
@@ -33,6 +33,8 @@ public class AccessModifiersExercises {
         System.out.println("💰 Saldo después del retiro: " + bankAccount.getBalance());
 
         // 4. Crea una clase Book con el atributo privado title. Permite leerlo con el método getTitle() pero no modificarlo (sin setTitle()). El título debe asignarse solo por el constructor.
+        Book book = new Book("Una Vida Extraordinaria");
+        System.out.println("📖 Titulo del libre: " + book.getTitle());
 
         // 5. Crea una clase Temperature con el atributo privado celsius. El método setCelsius(double celsius) solo debe aceptar valores entre -100 y 100.
 
---
+++ FirstClasses/Clase_08/Exercises_AccessModifiers/Book.java
@@ -0,0 +1,17 @@
+package Clase_08.Exercises_AccessModifiers;
+
+public class Book {
+
+    // Atributos
+    private String title;
+
+    // Constructor
+    public Book(String title) {
+        this.title = title;
+    }
+
+    // Métodos
+    public String getTitle(){
+        return this.title;
+    }
+}

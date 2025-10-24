7907c27f096cc1e759bc17919e2e4ed040cd9023
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-24 12:55:17 -0500

    Logic

---
+++ FirstClasses/Clase_08/inheritance.java
@@ -0,0 +1,19 @@
+package Clase_08;
+
+public class inheritance {
+    public static void main(String[] args) {
+
+        // Herencia
+
+    }
+
+    public static class Animal {
+        // Atributos
+        private String name;
+
+        // Constructor
+        public Animal(String name) {
+            this.name = name;
+        }
+    }
+}

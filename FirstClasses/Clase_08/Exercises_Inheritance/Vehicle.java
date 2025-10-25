c3a9640c9c31688dd40ab387f50a48c52dce0914
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-25 14:35:09 -0500

    Logic

---
+++ FirstClasses/Clase_08/Exercises_Inheritance/Vehicle.java
@@ -0,0 +1,10 @@
+package Clase_08.Exercises_Inheritance;
+
+public class Vehicle {
+
+    // Método
+    public void move() {
+        System.out.println("El vehículo se está moviendo...");
+    }
+}
+

c572aaec23253c985bb80fe94d85382592970c5c
AndresDev20 <andresmarroquin887@gmail.com>
2025-10-29 15:47:51 -0500

    Logic

--- FirstClasses/Clase_08/Polymorphism.java
+++ FirstClasses/Clase_08/Polymorphism.java
@@ -2,7 +2,41 @@ package Clase_08;
 
 public class Polymorphism {
 
-    // Polimorfismo
+    public static void main(String[] args) {
+
+        // Polimorfismo
+        // - Polimorfismo por herencia(sobrescritura)
+        var animal = new Animal();
+        var dog = new Dog();
+        animal.sound();
+        dog.sound();
+
+        // Polimorfismo por sobrecarga (sobrecarga de métodos)
+        var calculator = new Calculator();
+        System.out.println(calculator.sum(3, 5));
+        System.out.println(calculator.sum(3.2, 5.9));
+    }
+    public static class Animal {
+
+        public void sound() {
+            System.out.println("Algún sonido");
+        }
+    }
+    public static class Dog extends Animal {
+        @Override
+        public void sound() {
+            super.sound();
+            System.out.println("Guauuuuu!!!");
+        }
+    }
 
 
+    public static class Calculator {
+        public int sum( int a, int b) {
+            return a + b;
+        }
+        public double sum(double a, double b) {
+            return a + b;
+        }
+    }
 }

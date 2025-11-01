5377c6af0b416d2d7e9963ae9077f9f866efd483
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-01 14:17:47 -0500

    Logic

--- FirstClasses/Clase_08/Exercises_Polymorphism/Animal.java
+++ FirstClasses/Clase_08/Exercises_Polymorphism/Animal.java
@@ -2,4 +2,8 @@ package Clase_08.Exercises_Polymorphism;
 
 public class Animal {
 
+    // Métodos
+    public void makeSound() {
+        System.out.println("El animal tendrá que hacer un sonido");
+    }
 }

c6da7f98229419b35335194b460bf034ef92ecaf
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-02 15:44:21 -0500

    Logic

--- FirstClasses/Clase_08/Exercises_Polymorphism/Polymorphism.java
+++ FirstClasses/Clase_08/Exercises_Polymorphism/Polymorphism.java
@@ -88,4 +88,7 @@ public class Polymorphism {
             return base * height;
         }
     }
+
+    // Tercer ejercicio
+
 }
---
+++ FirstClasses/Clase_08/Exercises_Polymorphism/Printer.java
@@ -0,0 +1,9 @@
+package Clase_08.Exercises_Polymorphism;
+
+public class Printer {
+
+    // Métodos
+    public int suma(int a, int b) {
+        return a + b;
+    }
+}

6948df77a02d3cb070e64a686f726d18dd29ecc4
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-02 15:56:43 -0500

    Logic

--- FirstClasses/Clase_08/Exercises_Polymorphism/Greeter.java
+++ FirstClasses/Clase_08/Exercises_Polymorphism/Greeter.java
@@ -6,4 +6,7 @@ public class Greeter {
     public void greet() {
         System.out.println("Hello");
     }
+    public void greet(String name) {
+        System.out.println("Hello " + name);
+    }
 }

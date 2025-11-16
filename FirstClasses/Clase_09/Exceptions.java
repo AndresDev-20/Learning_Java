006694f229e5e3931ff8c89983eee4f46cb0d76b
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-16 11:59:19 -0500

    Logic

--- FirstClasses/Clase_09/Exceptions.java
+++ FirstClasses/Clase_09/Exceptions.java
@@ -1,7 +1,10 @@
 package Clase_09;
 
 public class Exceptions {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        var result = 10 / 0;
+        System.out.println(result);
     }
+
 }

3cca135b712fa0cdf2c023a0112c0f01df970770
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-16 12:23:02 -0500

    Logic

--- FirstClasses/Clase_09/Exceptions.java
+++ FirstClasses/Clase_09/Exceptions.java
@@ -3,8 +3,16 @@ package Clase_09;
 public class Exceptions {
 
     public static void main(String[] args) {
-        var result = 10 / 0;
-        System.out.println(result);
+        // Manejo de excepciones
+        try {
+            var result = 10 / 0;
+            System.out.println(result);
+        } catch () {
+
+        }
+        System.out.println("Fin");
+
+
     }
 
 }

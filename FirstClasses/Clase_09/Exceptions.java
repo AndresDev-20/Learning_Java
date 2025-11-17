b3bf1f2f1808ff075d6cbb32f78b6348760fbbb8
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-17 12:41:45 -0500

    Logic

--- FirstClasses/Clase_09/Exceptions.java
+++ FirstClasses/Clase_09/Exceptions.java
@@ -4,11 +4,12 @@ public class Exceptions {
 
     public static void main(String[] args) {
         // Manejo de excepciones
+
+        // try catch
         try {
             var result = 10 / 5;
-            String name = "Andres";
             System.out.println(result);
-        } catch (RuntimeException e) {
+        } catch (ArithmeticException e) {
             System.out.println("Error en el código: " + e);
         }
         System.out.println("Fin");

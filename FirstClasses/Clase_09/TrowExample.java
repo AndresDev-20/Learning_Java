44135e1126a6b825a652726a84b5ccf5b335aebb
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-18 16:05:25 -0500

    Logic

--- FirstClasses/Clase_09/TrowExample.java
+++ FirstClasses/Clase_09/TrowExample.java
@@ -1,9 +1,13 @@
 package Clase_09;
 
 public class TrowExample {
+    // Atributos
+    int age = 17;
 
     // Métodos o Funciones
     public void checkAge() {
+        if (age <= 18) {
 
+        }
     }
 }

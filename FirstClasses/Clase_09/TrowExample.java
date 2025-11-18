5012f9392fb33a5dfb0e89497f1592e3e109faac
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-18 15:50:40 -0500

    Logic

--- FirstClasses/Clase_09/TrowExample.java
+++ FirstClasses/Clase_09/TrowExample.java
@@ -3,6 +3,9 @@ package Clase_09;
 public class TrowExample {
 
     public static void main(String[] args) {
-
+        var age = 17;
+        if (age <= 18) {
+            throw new RuntimeException("Tienes que ser mayor de edad...");
+        }
     }
 }

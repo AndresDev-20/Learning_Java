84bf167467e9e91e16946d87293dcb8ca8c405f3
AndresDev20 <andresmarroquin887@gmail.com>
2025-11-19 17:20:10 -0500

    Logic

--- FirstClasses/Clase_10/Extras.java
+++ FirstClasses/Clase_10/Extras.java
@@ -5,6 +5,8 @@ public class Extras {
     public static void main(String[] args) {
         // Temas Extras
         // - null
-
+        String name = "Andres.dev";
+        name = null;
+        System.out.println(name);
     }
 }

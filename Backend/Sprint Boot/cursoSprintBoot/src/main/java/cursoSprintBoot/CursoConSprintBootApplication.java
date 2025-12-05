60e8461e8fddac5555648a94e2c9fecd6e29030a
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-05 14:24:49 -0500

    Logic

---
+++ Backend/Sprint Boot/cursoSprintBoot/src/main/java/cursoSprintBoot/CursoConSprintBootApplication.java
@@ -0,0 +1,13 @@
+package cursoSprintBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoConSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoConSprintBootApplication.class, args);
+	}
+
+}
---
+++ Backend/Sprint Boot/cursoSprintBoot/src/test/java/cursoSprintBoot/CursoConSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cursoSprintBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoConSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

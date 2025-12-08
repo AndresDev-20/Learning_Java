818bbec9b3d274800f7146f6a67cadbd99446700
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-08 11:56:30 -0500

    Spring Boot

--- Backend/Sprint Boot/cursoSprintBoot/src/main/java/cursoSprintBoot/controllers/HelloWorldRestController.java
+++ Backend/Sprint Boot/cursoSprintBoot/src/main/java/cursoSprintBoot/controllers/HelloWorldRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldRestController {
 
     // Métodos
-    @GetMapping({"/hello", "/hw"})
+    @GetMapping({"/hello", "/hw", "/hola"})
     public String helloWorld(){
         return "Hello, World";
     }

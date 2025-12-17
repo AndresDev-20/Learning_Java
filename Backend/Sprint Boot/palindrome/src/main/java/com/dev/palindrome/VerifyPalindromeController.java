42b3d8e69e1e534d26f5cc02a472105f855dce2d
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-17 15:09:59 -0500

    Spring Boot

--- Backend/Sprint Boot/palindrome/src/main/java/com/dev/palindrome/VerifyPalindromeController.java
+++ Backend/Sprint Boot/palindrome/src/main/java/com/dev/palindrome/VerifyPalindromeController.java
@@ -1,9 +1,21 @@
 package com.dev.palindrome;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VerifyPalindromeController {
 
     // Métodos
-    public String isPalindrome(String world){
-        return "";
+    @GetMapping("/verify-palindrome/{world}")
+    public String isPalindrome(@PathVariable String world){
+        StringBuilder sb = new StringBuilder(world);
+        sb.reverse();
+        if (world.equals(sb.toString())) {
+            return "La palabra " + world + " es un Palindrome";
+        } else {
+            return "La palabra " + world + " No es un Palindrome";
+        }
     }
 }

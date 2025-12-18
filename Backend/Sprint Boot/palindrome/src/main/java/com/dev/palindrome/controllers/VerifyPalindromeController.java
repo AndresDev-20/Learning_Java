c6ef5976b1b634c46c31d4f60fea73c18599dcc0
AndresDev20 <andresmarroquin887@gmail.com>
2025-12-18 14:02:09 -0500

    Spring Boot

--- Backend/Sprint Boot/palindrome/src/main/java/com/dev/palindrome/controllers/VerifyPalindromeController.java
+++ Backend/Sprint Boot/palindrome/src/main/java/com/dev/palindrome/controllers/VerifyPalindromeController.java
@@ -4,9 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * Controlador para verificar palíndromos
+ */
 @RestController
 public class VerifyPalindromeController {
 
+    /**
+     * Endpoint para verificar si una palabra es palíndromo
+     * @param w La palabra a verificar
+     * @return Un mensaje indicando si la palabra es palíndromo o no.
+     */
     // Métodos
     @GetMapping("/verify-palindrome/{w}")
     public String isPalindrome(@PathVariable String w){
